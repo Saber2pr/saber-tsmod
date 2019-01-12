1048284d466bcc3b5016cfd310e465725a1e9b05
saber2pr <saber2pr@gmail.com>
2019-01-12 22:03:48 +0000

    update

--- src/config/path.config.ts
+++ src/config/path.config.ts
@@ -2,8 +2,8 @@
  * @Author: AK-12
  * @Date: 2019-01-11 22:20:22
  * @Last Modified by: AK-12
- * @Last Modified time: 2019-01-12 13:38:12
+ * @Last Modified time: 2019-01-12 22:02:58
  */
-export const root = `${process.cwd}/src`
+export const root = `${process.cwd()}/src`
 export const core = `${root}/core`
 export const test = `${root}/test`
