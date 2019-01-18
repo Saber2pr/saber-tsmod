4e113ea2b6ee4d485c1a0cfae98d5482351dbf1c
saber2pr <saber2pr@gmail.com>
2019-01-18 10:37:50 +0000

    alter: template/module

--- src/core/template/module.ts
+++ src/core/template/module.ts
@@ -2,12 +2,12 @@
  * @Author: AK-12
  * @Date: 2019-01-15 13:53:03
  * @Last Modified by: AK-12
- * @Last Modified time: 2019-01-15 13:55:10
+ * @Last Modified time: 2019-01-18 10:36:54
  */
 export const module = (name: string, variable: string) => ({
   core: `export let ${variable} = '${name}'`,
   test: `import { ${variable} } from '../core/${name}'\n
 export function test_${variable}(){
-  alert(${variable})
+  typeof alert !== 'undefined' ? alert(${variable}) : console.log(${variable})
 }`
 })
